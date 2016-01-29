1d96c204200956d9827523d255e92122494e4ad6
karthik972 <kkeya4848@gmail.com>
2016-01-29 10:24:42 +0530

    Update ReadArguments.java
    Adding comments

--- code/ReadArguments.java
+++ code/ReadArguments.java
@@ -3,6 +3,7 @@ package practise;
 public class ReadArguments {
 
 	public static void main(String[] args) {
+		//Given command line arguments are stored as Array of String in args variable.
 		System.out.println("Total number of arguments :" + args.length);
 		System.out.println("Given Arguments are :");
 		for(int i=0; i<args.length; i++){
